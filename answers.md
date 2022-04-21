fc6eb0e7b297412a3590c1765352482ce1d7f014
Ruoqin <55730329+jiruoqin@users.noreply.github.com>
2022-04-20 22:43:42 -0500

    done

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Recitation 7
 ## Answers
 
-**Name:**_________________________
+**Name:** Ruoqin Ji
 
 
 Place all written answers from `recitation-07.md` here for easier grading.
@@ -12,15 +12,15 @@ Place all written answers from `recitation-07.md` here for easier grading.
 
 File | Fixed-Length Coding | Huffman Coding | Huffman vs. Fixed-Length
 ----------------------------------------------------------------------
-f1.txt    |                     |                |
-alice29.txt    |                     |                |
-asyoulik.txt    |                     |                |
-grammar.lsp    |                     |                |
-fields.c    |                     |                |
+f1.txt    |          1340           |        826        |  0.62
+alice29.txt    |         1039367            |        676374        |  0.65
+asyoulik.txt    |         876253            |        606448        |  0.69
+grammar.lsp    |          26047           |        17356        |  0.67
+fields.c    |         78050            |       56206         |  0.72
 
-
+The ratio of Huffman coding cost to fixed-length coding cost is about 0.67. 
 
 
 - **e.**
-
-
+If evert character in the alphabet $\sum$ has the same frequency. The expected cost of a Huffman encoding should be about the same as the cost of fixed-length encoding. 
+I test 3 files with same-frequency characters, the ratios of Huffman encoding to fixed-length are respectly 0.98, 0.85, 1. 
--- main.py
+++ main.py
@@ -1,12 +1,13 @@
+from ast import NodeTransformer
 import math, queue
 from collections import Counter
 
 class TreeNode(object):
     # we assume data is a tuple (frequency, character)
-    def __init__(self, left=None, right=None, data=None):
+    def __init__(self, left=None, right=None, data=None, direction=""):
         self.left = left
         self.right = right
-        self.data = data
+        self.data = data 
     def __lt__(self, other):
         return(self.data < other.data)
     def children(self):
@@ -34,27 +35,48 @@ def make_huffman_tree(f):
     # create a new node z with x and y as children,
     # insert z into the priority queue (using an empty character "")
     while (p.qsize() > 1):
-        # TODO
-        
+        l = p.get()
+        r = p.get()
+        p.put(TreeNode(r, l, (l.data[0]+r.data[0], "")))
+
+
     # return root of the tree
     return p.get()
 
 # perform a traversal on the prefix code tree to collect all encodings
+
 def get_code(node, prefix="", code={}):
-    # TODO - perform a tree traversal and collect encodings for leaves in code
-    pass
+    # perform a tree traversal and collect encodings for leaves in code
+    if(node.left):
+        get_code(node.left, prefix + "0")
+    
+    if(node.right):
+        get_code(node.right, prefix + "1")
+    
+    if (not node.left and not node.right):
+        code[node.data[1]] = prefix
+    
+    return code
+    
+
 
 # given an alphabet and frequencies, compute the cost of a fixed length encoding
 def fixed_length_cost(f):
-    # TODO
-    pass
-
+    totalLength = 0
+    for i, j in f.items():
+        oneLength = j
+        totalLength += oneLength 
+    return (math.ceil(math.log(len(f), 2))) * totalLength
 # given a Huffman encoding and character frequencies, compute cost of a Huffman encoding
 def huffman_cost(C, f):
-    # TODO
-    pass
+    totalLength = 0
+    for i, j in f.items():
+        oneLength = len(C[i]) * j
+        totalLength += oneLength
+    return totalLength
 
-f = get_frequencies('f1.txt')
+f = get_frequencies('same2.txt')
+#f = {'A': 9, 'B': 3, 'C': 2, 'D': 1}
 print("Fixed-length cost:  %d" % fixed_length_cost(f))
 T = make_huffman_tree(f)
 C = get_code(T)
